3870f4b1311cc8d62ff1601d5c7360242a179fc9
ivan.milic <ivan.milic@colorado.edu>
2019-04-30 15:45:49 -0600

    Changed plot_stokes_cube.py

--- pyplots/plot_stokes_cube.py
+++ pyplots/plot_stokes_cube.py
@@ -41,9 +41,8 @@ x = np.linspace(0.0,NX-1*1.0,NX)
 x *= 20.8 / 1E3
 y = x
 
-l_line = lines[4]
+l_line = lines[0]
 
-stokes_cube=np.transpose(stokes_cube,(1,0,2,3))
 
 plt.figure(figsize=[4.5*1, 3.7*2])
 plt.clf()
@@ -63,7 +62,6 @@ plt.ylabel('$y\,[\mathrm{Mm}]$')
 #plt.tight_layout()
 plt.savefig(sys.argv[2],bbox_inches='tight')
 plt.savefig(sys.argv[2]+'.eps',fmt='eps',bbox_inches='tight')
-quit();
 
 
 plt.clf()
@@ -79,12 +77,6 @@ for l in range(0,N_y):
 plt.tight_layout()
 plt.savefig(sys.argv[2],fmt='png',bbox_inches='tight')
 
-quit();
-i1 = 150
-j1 = 240
-
-i2 = 65
-j2 = 130
 
 
 #plt.clf()
@@ -97,14 +89,16 @@ j2 = 130
 #plt.ylabel("Normalized $I$")
 #plt.savefig('spectra_'+str(i1)+'_'+str(j1),fmt='png',bbox_inches='tight')
 
-l1 = 210
-l2 = 210
-to_plot=range(210,320)+range(480,530)+range(750,900)
+to_plot=range(0,NL)
 
 
 wvl = np.linspace(5887.7914658,5897.18400651,NL)
 
 mean = np.mean(stokes_cube[:,:,0,:],axis=(0,1))
+for s in range(1,4):
+	for l in to_plot:
+		stokes_cube[:,:,s,l] /= stokes_cube[:,:,0,0]
+stokes_cube[:,:,0,:] /= mean[0]
 plt.close('all')
 counter =0
 for l in to_plot:
@@ -112,21 +106,28 @@ for l in to_plot:
 	plt.figure(figsize=[8.5,5.5])
 	plt.clf()
 	plt.cla()
-	plt.subplot(311)
+	plt.subplot(221)
 	to_show = stokes_cube[:,:,0,l]
 	m = np.mean(to_show)
 	s = np.std(to_show)
 	plt.imshow(stokes_cube[:,:,0,l],origin='lower',cmap='hot',vmin=m-2*s,vmax=m+2*s)
 	plt.title('Intensity')
-	plt.subplot(312)
-	plt.imshow(np.mean(stokes_cube[:,:,3,l-2:l+2],axis=2),origin='lower',vmin=-0.1,vmax=0.1,cmap='coolwarm')
+	plt.subplot(222)
+	plt.imshow(stokes_cube[:,:,3,l],origin='lower',vmin=-0.1,vmax=0.1,cmap='coolwarm')
 	scalebar = ScaleBar(38.0*1E3) # 1 pixel = 0.2 meter
 	plt.gca().add_artist(scalebar)
 	plt.tight_layout()
 	plt.title('Circular polarization')
-	plt.subplot(313)
+	plt.subplot(223)
+	plt.imshow(np.sqrt(stokes_cube[:,:,1,l]**2.0+stokes_cube[:,:,2,l]**2.0),origin='lower',vmin=0.0,vmax=0.1,cmap='summer')
+	scalebar = ScaleBar(38.0*1E3) # 1 pixel = 0.2 meter
+	#plt.gca().add_artist(scalebar)
+	plt.tight_layout()
+	plt.title('Linear polarization')
+	
+	plt.subplot(224)
 	plt.plot(wvl,mean/max(mean),color='navy')
-	plt.ylim([0,1.1])
+	plt.ylim([0.4,1.1])
 	plt.xlim([wvl[0],wvl[-1]])
 	plt.xlabel('$\\lambda\,[\mathrm{\AA}]$')
 	plt.ylabel('Mean intensity')
