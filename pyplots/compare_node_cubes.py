28282cd604f673e88714a30658e9c2b264c61d64
ivan.milic <ivan.milic@colorado.edu>
2020-02-25 18:35:57 -0700

    reworking node compariing plot routine to fit all the panels in one plot

--- pyplots/compare_node_cubes.py
+++ pyplots/compare_node_cubes.py
@@ -26,6 +26,7 @@ plt.clf()
 plt.cla()
 
 params = [0,1,2,3,4,5,6,7,8,9,10,11]
+
 #cmaps = ['inferno','inferno','inferno','inferno','inferno','inferno','bwr','bwr','bwr','bwr','PRGn','PRGn']
 #unit = ['K','K','km/s','km/s','Gauss','Gauss']
 cmaps = [None] * 13
@@ -51,46 +52,72 @@ cubename2 = 'CNN'
 x = np.linspace(0,287,288)*20.8/1E3
 y = np.linspace(0,287,288)*20.8/1E3
 
-index = 0
-for p in params:
-	if (cmaps[index] == 'bwr'):
+
+to_plot = [2,3,8,10,11]
+NP = len(to_plot)
+plt.clf()
+plt.cla()
+scale = 3.4
+ratio = 0.93
+fig, axes = plt.subplots(nrows=NP,ncols=3,figsize=[scale*3,scale*NP*ratio])
+image_no = 0
+
+for i in range (0,len(to_plot)):
+	
+	p = to_plot[i]
+	# if it is the los velocity, scale and orient it properly
+	if (cmaps[p] == 'bwr'):
 		cube1[p] /= -1E5
 		cube2[p] /= -1E5
-	plt.figure(figsize=[12.0,4.0])
-	plt.clf()
-	plt.cla()
-	plt.subplot(1,3,1)
+	
 	m = np.mean(cube1[p])
 	s = np.std(cube1[p])
-	plt.imshow(cube1[p],vmin=m-3*s,vmax=m+3*s,cmap=cmaps[index],origin='lower',extent=[x[0],x[-1],y[0],y[-1]])
-	plt.title(cubename1+' ['+str(unit[p])+']')
-	plt.xlabel('$x\,\mathrm{[Mm]}$')
-	plt.ylabel('$y\,\mathrm{[Mm]}$')
-	plt.colorbar(shrink=0.8)
-	plt.subplot(1,3,2)
-	plt.imshow(cube2[p],vmin=m-3*s,vmax=m+3*s,cmap=cmaps[index],origin='lower',extent=[x[0],x[-1],y[0],y[-1]])
-	plt.xlabel('$x\,\mathrm{[Mm]}$')
-	plt.title(cubename2+' ['+str(unit[p])+']')
-	plt.colorbar(shrink=0.8)
-	plt.subplot(1,3,3)
-	#plt.hist(cube1[p].flatten(),bins=100,alpha=0.5,color='red')
-	#plt.hist(cube2[p].flatten(),bins=100,alpha=0.5,color='blue')
-	#plt.subplot(224)
-	plt.hist2d(cube1[p].flatten(),cube2[p].flatten(),bins=(100,100),cmap='Purples',vmax=30,range=[[m-3*s,m+3*s],[m-3*s,m+3*s]])
-	plt.plot(cube1[p].flatten(),cube1[p].flatten(),color='red')
-	plt.xlabel(cubename1)
-	plt.ylabel(cubename2)
-	r = stats.pearsonr(cube1[p].flatten(),cube2[p].flatten())
-	r = np.asarray(r)
-	r[1] = np.std(cube1[p].flatten()-cube2[p].flatten())
-	r[0] = round(r[0],3)
-	r[1] = round(r[1],3)
-	plt.text(m-3*s,m-2.8*s,'r='+str(r[0])+' $\\sigma=$'+str(r[1])+''+unit[index],fontsize=14)
-	print (r)
-	plt.tight_layout()
-	plt.savefig(sys.argv[3]+'_'+str(p)+'_.png',fmt='png',bbox_inches='tight')
-	plt.savefig(sys.argv[3]+'_'+str(p)+'_.eps',fmt='eps',bbox_inches='tight')
-	index +=1
+	
+	ax = axes.flat[image_no]
+	im = ax.imshow(cube1[p],vmin=m-3*s,vmax=m+3*s,cmap=cmaps[p],origin='lower',extent=[x[0],x[-1],y[0],y[-1]])
+	if (i==0):
+		ax.set_title('Standard Inversion')
+	if (i==NP-1):
+		ax.set_xlabel('$x\,\mathrm{[Mm]}$')
+	ax.set_ylabel('$y\,\mathrm{[Mm]}$')
+	if (i!=NP-1):
+		ax.set_xticklabels([])
+	
+	image_no +=1
+	ax = axes.flat[image_no]
+	im = ax.imshow(cube2[p],vmin=m-3*s,vmax=m+3*s,cmap=cmaps[p],origin='lower',extent=[x[0],x[-1],y[0],y[-1]])
+	if (i==0):
+		ax.set_title('CNN')
+	if (i==NP-1):
+		ax.set_xlabel('$x\,\mathrm{[Mm]}$')
+	if (i!=NP-1):
+		ax.set_xticklabels([])
+	ax.set_yticklabels([])
+	ax.colorbar(shrink=0.8)
+
+	image_no +=1
+	ax = axes.flat[image_no]
+	im = ax.hist2d(cube1[p].flatten(),cube2[p].flatten(),bins=(100,100),cmap='Purples',vmax=30,range=[[m-3*s,m+3*s],[m-3*s,m+3*s]])
+	ax.plot(cube1[p].flatten(),cube1[p].flatten(),color='red')
+	if (i==NP-1):
+		ax.set_xlabel(cubename1)
+	
+	ax.set_ylabel(cubename2)
+	image_no +=1
+	#r = stats.pearsonr(cube1[p].flatten(),cube2[p].flatten())
+	#r = np.asarray(r)
+	difference = (cube1[p].flatten()-cube2[p].flatten())
+	median = np.percentile(difference,50)
+	up = np.percentile(difference,95) - median
+	down = median - np.percentile(difference,5)
+	print (p,median,down,up)
+	#r[0] = round(r[0],3)
+	#r[1] = round(r[1],3)
+	#plt.text(m-3*s,m-2.8*s,'r='+str(r[0])+' $\\sigma=$'+str(r[1])+''+unit[index],fontsize=14)
+	#print (r)
+fig.tight_layout()
+fig.savefig(sys.argv[3]+'.png',fmt='png',bbox_inches='tight')
+fig.savefig(sys.argv[3]+'.eps',fmt='eps',bbox_inches='tight')
 
 #Here we plot correlations of correlations
 
